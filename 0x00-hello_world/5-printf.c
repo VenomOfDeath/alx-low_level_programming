17e8391b004c1afa4938ed5f2d676e03b56c3f6d
VenomOfDeath <mosesudoidua@gmail.com>
2022-09-08 17:39:02 -0700

    More fixes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - main block
+ * main - entry point
  * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
