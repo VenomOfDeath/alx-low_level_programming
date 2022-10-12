3d6cd0cbe127f8dd3b7cfa3f5a5543cf4d7c471b
VenomOfDeath <mosesudoidua@gmail.com>
2022-10-12 13:26:42 -0700

    0x0F. C - Function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,25 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add two integers
+ * @a: integer variable
+ * @b: integer variable
+ *
+ * Return: integer, sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract two integers
+ * @a: integer variable
+ * @b: integer variable
+ *
+ * Return: integer, difference between a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
